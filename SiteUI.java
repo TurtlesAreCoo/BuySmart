7bb2731278339b78b4ef635f50df15d64b289c74
TurtlesAreCoo <KingsleyL.0120@gmail.com>
2019-03-05 20:15:18 -0500

    Update SiteUI.java

--- SiteUI.java
+++ SiteUI.java
@@ -1,3 +1,29 @@
 public class SiteUI {
-	
+	CustomerDatabase customerList;
+	OrderDatabase orderList;
+
+	public SiteUI(){
+		customerList = new CustomerDatabase();
+		orderList = new OrderDatabase();
+	}
+
+	public void createOrder(){
+
+	}
+
+	public void processPayment(){
+
+	}
+
+	public void chooseShipping(){
+
+	}
+
+	public void updateCustomerDatabase(Customer newPerson){
+		customerList.addCustomer(newPerson);
+	}
+	public void updateOrderDatabase(Order newOrder){ 
+		orderList.addOrder(newOrder);
+	}
+
 }
